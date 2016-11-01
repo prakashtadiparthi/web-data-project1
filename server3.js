a37d929edc2499ad515a1fc5474e3094a56110c4
prakash <prakash.tadiparthi193@gmail.com>
2016-11-01 16:06:26 -0700

    semmons

--- server3.js
+++ server3.js
@@ -5,20 +5,26 @@ var fs = require("fs");
 
 
 app.use(express.static('public'));
-app.get('https://webdataproject1.herokuapp.com/index2.htm', function (req, res) {
+app.get('/index2.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index2.htm" );
 })
 
-app.get('https://webdataproject1.herokuapp.com/listUsers', function (req, res) {
+app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
       res.end(data);
    });
 })
 
-var server = app.listen(8080, function () {
+app.get('/listData', function (req, res) {
+   fs.readFile( __dirname + "/" + "sample_data.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end(data);
+   });
+})
+
+var server = app.listen(process.env.PORT||8080, function () {
    var host = server.address().address
-	//var host = "https://webdataproject1.herokuapp.com"
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
